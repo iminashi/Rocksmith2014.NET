b5495533be9307bd5209c1d0a6e74a9f4305a985
Tapio Malmberg <tapio.malmberg@gmail.com>
2020-07-24 10:14:17 +0300

    Add ToSng extension method

--- src/Rocksmith2014.Conversion/Extensions.fs
+++ src/Rocksmith2014.Conversion/Extensions.fs
@@ -1,2 +1,10 @@
-﻿module Rocksmith2014.Conversion.Extensions
+﻿namespace Rocksmith2014.Conversion
 
+open System.Runtime.CompilerServices
+open Rocksmith2014.XML
+
+[<Extension>]
+type InstrumentalArrangementExtension =
+    [<Extension>]
+    /// Converts the instrumental arrangement into SNG.
+    static member inline ToSng(arr: InstrumentalArrangement) = ConvertInstrumental.xmlToSng arr
