5a23686fe78ffa0dd3e1208b767a0f2c07e506f0
Tapio Malmberg <tapio.malmberg@gmail.com>
2020-08-07 23:19:32 +0300

    Refactoring

--- src/Rocksmith2014.DLCProject/AggregateGraph/AggregateGraph.fs
+++ src/Rocksmith2014.DLCProject/AggregateGraph/AggregateGraph.fs
@@ -2,54 +2,30 @@
 
 open Rocksmith2014.DLCProject
 open Rocksmith2014.Common
-open System
 open System.IO
 open System.Text
 
 type Graph = { Items: ResizeArray<GraphItem> }
 
 [<Literal>]
-let private CanonicalXBlock = "/gamexblocks/nsongs"
+let private canonicalXBlock = "/gamexblocks/nsongs"
 
 [<Literal>]
-let private CanonicalXmlSong = "/songs/arr"
-
-let private rand = Random()
-let private zeroes = Array.zeroCreate<byte> 8
-
-let private newLLID () = Guid(rand.Next(), 0s, 0s, zeroes)
-
-let private getPlatformTag = function
-    | PC -> Tag.DX9
-    | Mac -> Tag.MacOS
+let private canonicalXmlSong = "/songs/arr"
 
 let create (platform: Platform) (project: DLCProject) =
     let dlcName = project.DLCKey.ToLower()
 
     let items = ResizeArray<GraphItem>()
 
-    let xbl =
-        { Name = dlcName
-          Canonical = CanonicalXBlock
-          RelPath = sprintf "test/%s.xblock" dlcName
-          Tags = [ Tag.EmergentWorld; Tag.XWorld ]
-          UUID = Guid.NewGuid()
-          LogPath = None; LLID = None }
+    let xbl = GraphItem.normal dlcName canonicalXBlock "xblock" [ Tag.EmergentWorld; Tag.XWorld ]
     items.Add(xbl)
 
     for arrangement in project.Arrangements do
         match arrangement with 
         | Showlights ->
-            let sl =
-                let name = sprintf "%s_showlights" dlcName
-                let path = sprintf "%s/%s.xml" CanonicalXmlSong name
-                { Name = name
-                  Canonical = CanonicalXmlSong
-                  RelPath = path
-                  LogPath = Some path
-                  Tags = [ Tag.Application; Tag.XML ]
-                  UUID = Guid.NewGuid()
-                  LLID = Some (newLLID()) }
+            let name = sprintf "%s_showlights" dlcName
+            let sl = GraphItem.llid name canonicalXmlSong "xml" [ Tag.Application; Tag.XML ]
             items.Add(sl)
 
         | _ ->
@@ -57,50 +33,25 @@ let create (platform: Platform) (project: DLCProject) =
 
             let json =
                 let canonical = sprintf "/manifests/songs_dlc_%s" dlcName
-                let path = sprintf "%s/%s.json" canonical name
-                { Name = name
-                  Canonical = canonical
-                  RelPath = path
-                  Tags = [ Tag.Database; Tag.JsonDB ]
-                  UUID = Guid.NewGuid()
-                  LogPath = None; LLID = None }
+                GraphItem.normal name canonical "json" [ Tag.Database; Tag.JsonDB ]
             items.Add(json)
 
             let sng =
                 let canonical = sprintf "/songs/bin/%s" (Platform.getPath platform 1)
-                let path = sprintf "%s/%s.sng" canonical name
-                { Name = name
-                  Canonical = canonical
-                  RelPath = path
-                  LogPath = Some path
-                  Tags = [ Tag.Application; Tag.MusicgameSong ]
-                  UUID = Guid.NewGuid()
-                  LLID = Some (newLLID()) }
+                GraphItem.llid name canonical "sng" [ Tag.Application; Tag.MusicgameSong ]
             items.Add(sng)
 
     let hsan =
         let name = sprintf "songs_dlc_%s" dlcName
         let canonical = sprintf "/manifests/songs_dlc_%s" dlcName
-        { Name = name
-          Canonical = canonical
-          RelPath = sprintf "%s/%s.hsan" canonical name
-          Tags = [ Tag.Database; Tag.HsanDB]
-          UUID = Guid.NewGuid()
-          LLID = None; LogPath = None }
+        GraphItem.normal name canonical "hsan" [ Tag.Database; Tag.HsanDB ]
     items.Add(hsan)
 
     [ sprintf "album_%s_256" dlcName; sprintf "album_%s_128" dlcName; sprintf "album_%s_64" dlcName  ]
     |> List.iter (fun art ->
         let dds =
             let canonical = "/gfxassets/album_art"
-            let path = sprintf "%s/%s.dds" canonical art
-            { Name = art
-              Canonical = canonical
-              RelPath = path
-              LogPath = Some path
-              Tags = [ Tag.DDS; Tag.Image ]
-              UUID = Guid.NewGuid()
-              LLID = Some (newLLID()) }
+            GraphItem.llid art canonical "dds" [ Tag.DDS; Tag.Image ]
         items.Add(dds))
 
     project.Arrangements
@@ -109,38 +60,17 @@ let create (platform: Platform) (project: DLCProject) =
         let dds =
             let name = sprintf "lyrics_%s" dlcName
             let canonical = sprintf "/assets/ui/lyrics/%s" dlcName
-            let path = sprintf "%s/%s.dds" canonical name
-            { Name = name
-              Canonical = canonical
-              RelPath = path
-              LogPath = Some path
-              Tags = [ Tag.DDS; Tag.Image ]
-              UUID = Guid.NewGuid()
-              LLID = Some (newLLID()) }
+            GraphItem.llid name canonical "dds" [ Tag.DDS; Tag.Image ]
         items.Add(dds))
 
     let bnkMain =
-        let canonical = sprintf "/audio/%s" (Platform.getPath platform 0)
         let name = sprintf "song_%s" dlcName
-        { Canonical = canonical 
-          Name = name 
-          RelPath = sprintf "%s/%s.bnk" canonical name
-          LogPath = Some(sprintf "/audio/%s.bnk" dlcName)
-          Tags = [ Tag.Audio; Tag.WwiseSoundBank; getPlatformTag platform ]
-          UUID = Guid.NewGuid()
-          LLID = Some(newLLID()) }
+        GraphItem.bnk name platform
     items.Add(bnkMain)
 
     let bnkPreview =
-        let canonical = sprintf "/audio/%s" (Platform.getPath platform 0)
         let name = sprintf "song_%s_preview" dlcName
-        { Canonical = canonical 
-          Name = name 
-          RelPath = sprintf "%s/%s.bnk" canonical name
-          LogPath = Some(sprintf "/audio/%s.bnk" dlcName)
-          Tags = [ Tag.Audio; Tag.WwiseSoundBank; getPlatformTag platform ]
-          UUID = Guid.NewGuid()
-          LLID = Some(newLLID()) }
+        GraphItem.bnk name platform
     items.Add(bnkPreview)
 
     { Items = items }
--- src/Rocksmith2014.DLCProject/AggregateGraph/GraphItem.fs
+++ src/Rocksmith2014.DLCProject/AggregateGraph/GraphItem.fs
@@ -2,6 +2,7 @@
 
 open System
 open System.IO
+open Rocksmith2014.Common
 
 type GraphItem =
     { UUID: Guid
@@ -14,7 +15,40 @@ type GraphItem =
 
 module GraphItem =
     let private lineTemplate = "<urn:uuid:{0}> <http://emergent.net/aweb/1.0/{1}> \"{2}\"."
-
+    let private rand = Random()
+    let private zeroes = Array.zeroCreate<byte> 8
+    
+    let private newLLID () = Guid(rand.Next(), 0s, 0s, zeroes)
+    
+    let private getPlatformTag = function
+        | PC -> Tag.DX9
+        | Mac -> Tag.MacOS
+    
+    let private make name canonical tags rp lp =
+        let llid = lp |> Option.map (fun _ -> newLLID ())
+        { Name = name
+          Canonical = canonical
+          RelPath = rp; LogPath = lp
+          Tags = tags
+          UUID = Guid.NewGuid(); LLID = llid }
+    
+    /// Creates a graph item.
+    let normal name canonical extension tags =
+        let path = sprintf "%s/%s.%s" canonical name extension
+        make name canonical tags path None
+    
+    /// Creates a graph item with an LLID when relative path and logical paths are identical.
+    let llid name canonical extension tags =
+        let path = sprintf "%s/%s.%s" canonical name extension
+        make name canonical tags path (Some path)
+    
+    /// Creates a graph item for BNK files.
+    let bnk name platform =
+        let canonical = sprintf "/audio/%s" (Platform.getPath platform 0)
+        let rp = sprintf "%s/%s.bnk" canonical name
+        let lp = sprintf "/audio/%s.bnk" name |> Some
+        make name canonical [ Tag.Audio; Tag.WwiseSoundBank; getPlatformTag platform ] rp lp
+    
     let write (writer: StreamWriter) (item: GraphItem) =
         let uuid = item.UUID.ToString()
 
@@ -24,10 +58,11 @@ module GraphItem =
         writer.WriteLine(lineTemplate, uuid, TagType.Canonical, item.Canonical)
         writer.WriteLine(lineTemplate, uuid, TagType.Name, item.Name)
         
-        item.LLID
-        |> Option.iter (fun l -> writer.WriteLine(lineTemplate, uuid, TagType.LLID, l))
-        
-        item.LogPath
-        |> Option.iter (fun l -> writer.WriteLine(lineTemplate, uuid, TagType.LogPath, l))
+        match item.LLID, item.LogPath with
+        | Some llid, Some logPath ->
+            writer.WriteLine(lineTemplate, uuid, TagType.LLID, llid)
+            writer.WriteLine(lineTemplate, uuid, TagType.LogPath, logPath)
+        | None, None -> ()
+        | _ -> failwith "LLID and Log Path must both have a value."
         
         writer.Write(lineTemplate, uuid, TagType.RelPath, item.RelPath)
