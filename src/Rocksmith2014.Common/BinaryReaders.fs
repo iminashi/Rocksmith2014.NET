21871fa2a25ff23f88922e59252811fde3d69027
Tapio Malmberg <tapio.malmberg@gmail.com>
2020-08-15 17:19:29 +0300

    Make the binary readers work with slow streams

--- src/Rocksmith2014.Common/BinaryReaders.fs
+++ src/Rocksmith2014.Common/BinaryReaders.fs
@@ -8,147 +8,153 @@ open Interfaces
 
 #nowarn "9"
 
-// TODO: Error handling
-
 type LittleEndianBinaryReader(stream: Stream) =
     interface IBinaryReader with
         member _.ReadInt8() = stream.ReadByte() |> int8
 
-        member _.ReadInt16() =
+        member this.ReadInt16() =
             let length = 2
             let buffer = NativePtr.stackalloc<byte> length |> NativePtr.toVoidPtr
-            let bytesRead = stream.Read(Span<byte>(buffer, length))
+            (this :> IBinaryReader).ReadSpan(Span<byte>(buffer, length))
             BinaryPrimitives.ReadInt16LittleEndian(ReadOnlySpan(buffer, length))
 
-        member _.ReadUInt16() =
+        member this.ReadUInt16() =
             let length = 2
             let buffer = NativePtr.stackalloc<byte> length |> NativePtr.toVoidPtr
-            let bytesRead = stream.Read(Span<byte>(buffer, length))
+            (this :> IBinaryReader).ReadSpan(Span<byte>(buffer, length))
             BinaryPrimitives.ReadUInt16LittleEndian(ReadOnlySpan(buffer, length))
 
-        member _.ReadUInt24() =
+        member this.ReadUInt24() =
             let length = 4
             let buffer = NativePtr.stackalloc<byte> length |> NativePtr.toVoidPtr
             let span = Span<byte>(buffer, length)
-            let bytesRead = stream.Read(span.Slice(0,3))
+            (this :> IBinaryReader).ReadSpan(span.Slice(0,3))
             span.[3] <- 0uy
             BinaryPrimitives.ReadUInt32LittleEndian(ReadOnlySpan(buffer, length))
 
-        member _.ReadInt32() =
+        member this.ReadInt32() =
             let length = 4
             let buffer = NativePtr.stackalloc<byte> length |> NativePtr.toVoidPtr
-            let bytesRead = stream.Read(Span<byte>(buffer, length))
+            (this :> IBinaryReader).ReadSpan(Span<byte>(buffer, length))
             BinaryPrimitives.ReadInt32LittleEndian(ReadOnlySpan(buffer, length))
 
-        member _.ReadUInt32() =
+        member this.ReadUInt32() =
             let length = 4
             let buffer = NativePtr.stackalloc<byte> length |> NativePtr.toVoidPtr
-            let bytesRead = stream.Read(Span<byte>(buffer, length))
+            (this :> IBinaryReader).ReadSpan(Span<byte>(buffer, length))
             BinaryPrimitives.ReadUInt32LittleEndian(ReadOnlySpan(buffer, length))
         
-        member _.ReadUInt40() =
+        member this.ReadUInt40() =
             let length = 8
             let buffer = NativePtr.stackalloc<byte> length |> NativePtr.toVoidPtr
             let span = Span<byte>(buffer, length)
-            let bytesRead = stream.Read(span.Slice(0, 5))
+            (this :> IBinaryReader).ReadSpan(span.Slice(0, 5))
             span.[5] <- 0uy; span.[6] <- 0uy; span.[7] <- 0uy
             BinaryPrimitives.ReadUInt64LittleEndian(ReadOnlySpan(buffer, length))
 
-        member _.ReadUInt64() =
+        member this.ReadUInt64() =
             let length = 8
             let buffer = NativePtr.stackalloc<byte> length |> NativePtr.toVoidPtr
-            let bytesRead = stream.Read(Span<byte>(buffer, length))
+            (this :> IBinaryReader).ReadSpan(Span<byte>(buffer, length))
             BinaryPrimitives.ReadUInt64LittleEndian(ReadOnlySpan(buffer, length))
 
-        member _.ReadSingle() =
+        member this.ReadSingle() =
             let length = 4
             let buffer = NativePtr.stackalloc<byte> length |> NativePtr.toVoidPtr
-            let bytesRead = stream.Read(Span<byte>(buffer, length))
+            (this :> IBinaryReader).ReadSpan(Span<byte>(buffer, length))
             BitConverter.Int32BitsToSingle(BinaryPrimitives.ReadInt32LittleEndian(ReadOnlySpan(buffer, length)))
 
-        member _.ReadDouble() =
+        member this.ReadDouble() =
             let length = 8
             let buffer = NativePtr.stackalloc<byte> length |> NativePtr.toVoidPtr
-            let bytesRead = stream.Read(Span<byte>(buffer, length))
+            (this :> IBinaryReader).ReadSpan(Span<byte>(buffer, length))
             BitConverter.Int64BitsToDouble(BinaryPrimitives.ReadInt64LittleEndian(ReadOnlySpan(buffer, length)))
 
-        member _.ReadBytes(count) =
+        member this.ReadBytes(count) =
             let buffer = Array.zeroCreate<byte> count
-            let bytesRead = stream.Read(buffer.AsSpan())
+            (this :> IBinaryReader).ReadSpan(buffer.AsSpan())
             buffer
 
         member _.ReadSpan(span) =
-            stream.Read(span) |> ignore
+            let mutable bytesRead = stream.Read span
+            let mutable totalRead = bytesRead
+            while totalRead < span.Length && bytesRead <> 0 do
+                bytesRead <- stream.Read(span.Slice totalRead)
+                totalRead <- totalRead + bytesRead
 
 type BigEndianBinaryReader(stream: Stream) =
     interface IBinaryReader with
         member _.ReadInt8() = stream.ReadByte() |> int8
 
-        member _.ReadInt16() =
+        member this.ReadInt16() =
             let length = 2
             let buffer = NativePtr.stackalloc<byte> length |> NativePtr.toVoidPtr
-            let bytesRead = stream.Read(Span<byte>(buffer, length))
+            (this :> IBinaryReader).ReadSpan(Span<byte>(buffer, length))
             BinaryPrimitives.ReadInt16BigEndian(ReadOnlySpan(buffer, length))
 
-        member _.ReadUInt16() =
+        member this.ReadUInt16() =
             let length = 2
             let buffer = NativePtr.stackalloc<byte> length |> NativePtr.toVoidPtr
-            let bytesRead = stream.Read(Span<byte>(buffer, length))
+            (this :> IBinaryReader).ReadSpan(Span<byte>(buffer, length))
             BinaryPrimitives.ReadUInt16BigEndian(ReadOnlySpan(buffer, length))
 
-        member _.ReadUInt24() =
+        member this.ReadUInt24() =
             let length = 4
             let buffer = NativePtr.stackalloc<byte> length |> NativePtr.toVoidPtr
             let span = Span<byte>(buffer, length)
-            let bytesRead = stream.Read(span.Slice(1, 3))
+            (this :> IBinaryReader).ReadSpan(span.Slice(1, 3))
             span.[0] <- 0uy
             BinaryPrimitives.ReadUInt32BigEndian(ReadOnlySpan(buffer, length))
 
-        member _.ReadInt32() =
+        member this.ReadInt32() =
             let length = 4
             let buffer = NativePtr.stackalloc<byte> length |> NativePtr.toVoidPtr
-            let bytesRead = stream.Read(Span<byte>(buffer, length))
+            (this :> IBinaryReader).ReadSpan(Span<byte>(buffer, length))
             BinaryPrimitives.ReadInt32BigEndian(ReadOnlySpan(buffer, length))
 
-        member _.ReadUInt32() =
+        member this.ReadUInt32() =
             let length = 4
             let buffer = NativePtr.stackalloc<byte> length |> NativePtr.toVoidPtr
-            let bytesRead = stream.Read(Span<byte>(buffer, length))
+            (this :> IBinaryReader).ReadSpan(Span<byte>(buffer, length))
             BinaryPrimitives.ReadUInt32BigEndian(ReadOnlySpan(buffer, length))
 
-        member _.ReadUInt40() =
+        member this.ReadUInt40() =
              let length = 8
              let buffer = NativePtr.stackalloc<byte> length |> NativePtr.toVoidPtr
              let span = Span<byte>(buffer, length)
-             let bytesRead = stream.Read(span.Slice(3, 5))
+             (this :> IBinaryReader).ReadSpan(span.Slice(3, 5))
              span.[0] <- 0uy; span.[1] <- 0uy; span.[2] <- 0uy
              BinaryPrimitives.ReadUInt64BigEndian(ReadOnlySpan(buffer, length))
 
-        member _.ReadUInt64() =
+        member this.ReadUInt64() =
              let length = 8
              let buffer = NativePtr.stackalloc<byte> length |> NativePtr.toVoidPtr
-             let bytesRead = stream.Read(Span<byte>(buffer, length))
+             (this :> IBinaryReader).ReadSpan(Span<byte>(buffer, length))
              BinaryPrimitives.ReadUInt64BigEndian(ReadOnlySpan(buffer, length))
 
-        member _.ReadSingle() =
+        member this.ReadSingle() =
             let length = 4
             let buffer = NativePtr.stackalloc<byte> length |> NativePtr.toVoidPtr
-            let bytesRead = stream.Read(Span<byte>(buffer, length))
+            (this :> IBinaryReader).ReadSpan(Span<byte>(buffer, length))
             BitConverter.Int32BitsToSingle(BinaryPrimitives.ReadInt32BigEndian(ReadOnlySpan(buffer, length)))
 
-        member _.ReadDouble() =
+        member this.ReadDouble() =
             let length = 8
             let buffer = NativePtr.stackalloc<byte> length |> NativePtr.toVoidPtr
-            let bytesRead = stream.Read(Span<byte>(buffer, length))
+            (this :> IBinaryReader).ReadSpan(Span<byte>(buffer, length))
             BitConverter.Int64BitsToDouble(BinaryPrimitives.ReadInt64BigEndian(ReadOnlySpan(buffer, length)))
 
-        member _.ReadBytes(count) =
+        member this.ReadBytes(count) =
             let buffer = Array.zeroCreate<byte> count
-            let bytesRead = stream.Read(buffer.AsSpan())
+            (this :> IBinaryReader).ReadSpan(buffer.AsSpan())
             buffer
 
         member _.ReadSpan(span) =
-            stream.Read(span) |> ignore
+            let mutable bytesRead = stream.Read span
+            let mutable totalRead = bytesRead
+            while totalRead < span.Length && bytesRead <> 0 do
+                bytesRead <- stream.Read(span.Slice totalRead)
+                totalRead <- totalRead + bytesRead
 
 /// Returns a binary reader that matches the given platform.
 let getReader stream platform =
---
+++ tests/Rocksmith2014.Common.Tests/SlowStreamTests.fs
@@ -0,0 +1,121 @@
+﻿module SlowStreamTests
+
+open System
+open System.IO
+open Expecto
+open Rocksmith2014.Common.BinaryReaders
+open Rocksmith2014.Common.Interfaces
+
+/// A stream that returns data one byte at a time
+type SlowStream() =
+    inherit Stream()
+
+    let data = Array.init<byte> 40 byte
+    let mutable index = 0
+
+    override _.Read(buffer: Span<byte>) =
+        if index < 40 then
+            buffer.[0] <- data.[index]
+            index <- index + 1
+            1
+        else
+            0
+
+    override _.Read(buffer, offset, _count) =
+        if index < 40 then
+            buffer.[offset] <- data.[index]
+            index <- index + 1
+            1
+        else
+            0
+
+    override _.Write(_buffer, _offset, _count) = ()
+    override _.get_CanRead() = true
+    override _.get_CanWrite() = false
+    override _.get_CanSeek() = false
+    override _.get_Length() = int64 data.Length
+    override _.get_Position() = int64 index
+    override _.set_Position(pos) = index <- int pos
+    override _.SetLength(_length) = ()
+    override _.Flush() = ()
+    override _.Seek(_offset, _origin) = 0L
+
+[<Tests>]
+let bigEndianSlowStream =
+  testList "Big-Endian Binary Reader with Slow Stream" [
+    test "Can read bytes" {
+        use stream = new SlowStream()
+        let reader = BigEndianBinaryReader(stream) :> IBinaryReader
+        let expected = seq { 0uy; 1uy; 2uy; 3uy; 4uy }
+
+        let array = reader.ReadBytes 5
+
+        Expect.sequenceContainsOrder array expected "Sequence of 5 bytes is correct" }
+
+    test "Can read signed 16-bit integer" {
+        use stream = new SlowStream()
+        let reader = BigEndianBinaryReader(stream) :> IBinaryReader
+        let expected = BitConverter.ToInt16([| 1uy; 0uy |], 0)
+
+        let read = reader.ReadInt16()
+
+        Expect.equal read expected "Signed 16-bit integer read correctly" }
+
+    test "Can read unsigned 24-bit integer" {
+        use stream = new SlowStream()
+        let reader = BigEndianBinaryReader(stream) :> IBinaryReader
+        let expected = BitConverter.ToUInt32([| 2uy; 1uy; 0uy; 0uy |], 0)
+
+        let read = reader.ReadUInt24()
+
+        Expect.equal read expected "Unsigned 24-bit integer read correctly" }
+
+    test "Can read unsigned 64-bit integer" {
+        use stream = new SlowStream()
+        let reader = BigEndianBinaryReader(stream) :> IBinaryReader
+        let expected = BitConverter.ToUInt64([| 7uy; 6uy; 5uy; 4uy; 3uy; 2uy; 1uy; 0uy |], 0)
+
+        let read = reader.ReadUInt64()
+
+        Expect.equal read expected "Unsigned 64-bit integer read correctly" }
+  ]
+
+[<Tests>]
+let littleEndianSlowStream =
+  testList "Little-Endian Binary Reader with Slow Stream" [
+    test "Can read bytes" {
+        use stream = new SlowStream()
+        let reader = LittleEndianBinaryReader(stream) :> IBinaryReader
+        let expected = seq { 0uy; 1uy; 2uy; 3uy; 4uy }
+
+        let array = reader.ReadBytes 5
+
+        Expect.sequenceContainsOrder array expected "Sequence of 5 bytes is correct" }
+
+    test "Can read signed 16-bit integer" {
+        use stream = new SlowStream()
+        let reader = LittleEndianBinaryReader(stream) :> IBinaryReader
+        let expected = BitConverter.ToInt16([| 0uy; 1uy |], 0)
+
+        let read = reader.ReadInt16()
+
+        Expect.equal read expected "Signed 16-bit integer read correctly" }
+
+    test "Can read unsigned 24-bit integer" {
+        use stream = new SlowStream()
+        let reader = LittleEndianBinaryReader(stream) :> IBinaryReader
+        let expected = BitConverter.ToUInt32([| 0uy; 1uy; 2uy; 0uy |], 0)
+
+        let read = reader.ReadUInt24()
+
+        Expect.equal read expected "Unsigned 24-bit integer read correctly" }
+
+    test "Can read unsigned 64-bit integer" {
+        use stream = new SlowStream()
+        let reader = LittleEndianBinaryReader(stream) :> IBinaryReader
+        let expected = BitConverter.ToUInt64([| 0uy; 1uy; 2uy; 3uy; 4uy; 5uy; 6uy; 7uy |], 0)
+
+        let read = reader.ReadUInt64()
+
+        Expect.equal read expected "Unsigned 64-bit integer read correctly" }
+  ]
