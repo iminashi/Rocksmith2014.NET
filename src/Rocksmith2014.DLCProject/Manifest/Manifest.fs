d9584690cec42e2cbe601f34382a232b67d3bb97
Tapio Malmberg <tapio.malmberg@gmail.com>
2020-09-24 22:29:52 +0300

    [dlcproject] Add comments

--- src/Rocksmith2014.DLCProject/Manifest/Manifest.fs
+++ src/Rocksmith2014.DLCProject/Manifest/Manifest.fs
@@ -24,12 +24,15 @@ module Manifest =
           IterationVersion = iterationVersion |> Option.toNullable
           InsertRoot = insertRoot }
 
+    /// Creates a manifest with the given attributes.
     let create (attrs: Attributes list) =
         createInternal attrs (Some "RSEnumerable_Song") (Some 2) "Static.Songs.Entries"
 
+    /// Creates a manifest header with the given attributes.
     let createHeader (attrs: Attributes list) =
         createInternal attrs None None "Static.Songs.Headers"
 
+    /// Returns the attributes from a manifest expected to have only one entry.
     let getSingletonAttributes (manifest: Manifest) =
         manifest.Entries
         |> Map.toArray
@@ -42,18 +45,23 @@ module Manifest =
         o.Converters.Add(JsonFSharpConverter())
         o
 
+    /// Serializes the manifest into a JSON string.
     let toJsonString (manifest: Manifest) =
         JsonSerializer.Serialize(manifest, options)
 
+    /// Serializes the manifest as JSON into the output stream.
     let toJsonStream (output: Stream) (manifest: Manifest) =
         JsonSerializer.SerializeAsync(output, manifest, options)
 
+    /// Deserializes a manifest from a JSON string.
     let fromJsonString (str: string) =
         JsonSerializer.Deserialize<Manifest>(str, options)
 
+    /// Deserializes a manifest from a JSON stream.
     let fromJsonStream (input: Stream) =
         JsonSerializer.DeserializeAsync<Manifest>(input, options)
 
+    /// Deserializes a manifest from a file.
     let fromJsonFile (path: string) = async {
         use file = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read, 4096, FileOptions.SequentialScan ||| FileOptions.Asynchronous)
         return! fromJsonStream(file) }
