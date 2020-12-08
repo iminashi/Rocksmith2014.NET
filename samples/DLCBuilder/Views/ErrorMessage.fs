fb8545d3701f4bc60505922ad5c69f84d141d484
Tapio Malmberg <tapio.malmberg@gmail.com>
2020-12-08 17:56:41 +0200

    [dlcbuilder] Add alert icon

--- samples/DLCBuilder/Views/ErrorMessage.fs
+++ samples/DLCBuilder/Views/ErrorMessage.fs
@@ -1,30 +1,47 @@
 ﻿module DLCBuilder.ErrorMessage
 
 open Avalonia.FuncUI.DSL
-open Avalonia.Controls
-open Avalonia.Layout
 open Avalonia.FuncUI.Types
+open Avalonia.Controls
+open Avalonia.Controls.Shapes
+open Avalonia.Layout
+open Avalonia.Media
 
 let view state dispatch msg =
     StackPanel.create [
         StackPanel.spacing 8.
         StackPanel.children [
-            TextBlock.create [
-                TextBlock.fontSize 18.
-                TextBlock.text (state.Localization.GetString "error")
-                TextBlock.horizontalAlignment HorizontalAlignment.Center
+            StackPanel.create [
+                StackPanel.orientation Orientation.Horizontal
+                StackPanel.horizontalAlignment HorizontalAlignment.Center
+                StackPanel.children [
+                    Path.create [
+                        Path.fill Brushes.Gray
+                        Path.data Media.Icons.alert
+                        Path.verticalAlignment VerticalAlignment.Center
+                        Path.margin (0., 0., 14., 0.)
+                    ]
+                    TextBlock.create [
+                        TextBlock.fontSize 18.
+                        TextBlock.text (state.Localization.GetString "error")
+                    ]
+                ]
             ]
+            
+            // Message
             TextBlock.create [
                 TextBlock.fontSize 16.
                 TextBlock.text msg
                 TextBlock.margin 10.0
             ]
+
+            // OK button
             Button.create [
                 Button.fontSize 18.
                 Button.padding (80., 10.)
                 Button.horizontalAlignment HorizontalAlignment.Center
                 Button.content (state.Localization.GetString "ok")
-                Button.onClick (fun _ -> CloseOverlay |> dispatch)
+                Button.onClick (fun _ -> dispatch CloseOverlay)
             ]
         ]
     ] :> IView
