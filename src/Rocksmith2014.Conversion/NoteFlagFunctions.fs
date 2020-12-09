dc51444afb4aa1cd6222b585bb37a6175a91d1d6
Tapio Malmberg <tapio.malmberg@gmail.com>
2020-12-09 20:43:52 +0200

    [conversion] Improve pattern match

--- src/Rocksmith2014.Conversion/NoteFlagFunctions.fs
+++ src/Rocksmith2014.Conversion/NoteFlagFunctions.fs
@@ -4,14 +4,18 @@ open Rocksmith2014.SNG
 
 // Functions for flagging notes (0 = None, 1 = The note is numbered).
 
+/// Numbers all notes.
 let always (_: Note option) (currentNote: Note) =
     if currentNote.FretId <> 0y then 1u else 0u
 
+/// Never numbers notes.
 let never (_: Note option) (_: Note) = 0u
 
+/// Numbers a note when the anchor changes.
 let onAnchorChange (previousNote: Note option) (currentNote: Note) =
     match previousNote with
-    | None ->
-        if currentNote.FretId <> 0y then 1u else 0u
-    | Some previous ->
-        if previous.AnchorFretId <> currentNote.AnchorFretId && currentNote.FretId <> 0y then 1u else 0u
+    // Never number open notes
+    | _ when currentNote.FretId = 0y -> 0u
+    | Some previous when previous.AnchorFretId <> currentNote.AnchorFretId -> 1u
+    | Some _ -> 0u
+    | None -> 1u
