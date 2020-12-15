f32b3abd510c2052ee805fe0a14781f3db26ebec
Tapio Malmberg <tapio.malmberg@gmail.com>
2020-12-15 23:41:52 +0200

    Port addCrowdEvents from DDC Improver

---
+++ tests/Rocksmith2014.XML.Processing.Tests/ArrangementImproverTests.fs
@@ -0,0 +1,19 @@
+﻿module Rocksmith2014.XML.Processing.Tests.ArrangementImproverTests
+
+open Expecto
+open Rocksmith2014.XML
+open Rocksmith2014.XML.Processing
+
+[<Tests>]
+let eventTests =
+    testList "Arrangement Improver (Crowd Events)" [
+        testCase "Creates events" <| fun _ ->
+            let notes = ResizeArray(seq { Note(Time = 10000) })
+            let level = Level(Notes = notes)
+            let arr = InstrumentalArrangement(Levels = ResizeArray(seq { level }))
+            arr.MetaData.SongLength <- 120_000
+
+            ArrangementImprover.addCrowdEvents arr
+
+            Expect.isNonEmpty arr.Events "Events were created"
+    ]
