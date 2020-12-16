f5771ec8505fcb01fbd2fbb601f67a1cb521246b
Tapio Malmberg <tapio.malmberg@gmail.com>
2020-12-16 14:53:47 +0200

    Reorganize improver modules and add test

--- tests/Rocksmith2014.XML.Processing.Tests/ArrangementImproverTests.fs
+++ tests/Rocksmith2014.XML.Processing.Tests/ArrangementImproverTests.fs
@@ -5,9 +5,9 @@ open Rocksmith2014.XML
 open Rocksmith2014.XML.Processing
 
 [<Tests>]
-let eventTests =
+let crowdEventTests =
     testList "Arrangement Improver (Crowd Events)" [
-        testCase "Creates events" <| fun _ ->
+        testCase "Creates crowd events" <| fun _ ->
             let notes = ResizeArray(seq { Note(Time = 10000) })
             let level = Level(Notes = notes)
             let arr = InstrumentalArrangement(Levels = ResizeArray(seq { level }))
@@ -16,4 +16,15 @@ let eventTests =
             ArrangementImprover.addCrowdEvents arr
 
             Expect.isNonEmpty arr.Events "Events were created"
+
+        testCase "No events are created when already present" <| fun _ ->
+            let notes = ResizeArray(seq { Note(Time = 10000) })
+            let level = Level(Notes = notes)
+            let events = ResizeArray(seq { Event("e1", 1000); Event("E3", 10000); Event("D3", 20000) })
+            let arr = InstrumentalArrangement(Events = events, Levels = ResizeArray(seq { level }))
+            arr.MetaData.SongLength <- 120_000
+
+            ArrangementImprover.addCrowdEvents arr
+
+            Expect.hasLength arr.Events 3 "No new events were created"
     ]
