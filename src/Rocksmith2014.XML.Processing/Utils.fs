d8c76929bc6dcbd0177a33c4e773cd8c85fa99f4
Tapio Malmberg <tapio.malmberg@gmail.com>
2020-12-18 23:05:49 +0200

    Reduce duplicate code

--- src/Rocksmith2014.XML.Processing/Utils.fs
+++ src/Rocksmith2014.XML.Processing/Utils.fs
@@ -8,21 +8,17 @@ let timeToString time =
     let milliSeconds = time - (minutes * 60 * 1000) - (seconds * 1000)
     $"{minutes:D2}:{seconds:D2}.{milliSeconds:D3}"
 
-let findTimeOfNthNoteFrom (level: Level) (startTime: int) (nthNote: int) =
-    let noteTimes =
-        level.Notes
-        |> Seq.filter (fun n -> n.Time >= startTime)
-        |> Seq.map (fun n -> n.Time)
-        // Notes on the same timecode (i.e. split chords) count as one
-        |> Seq.distinct
-        |> Seq.truncate nthNote
+let private getTimes<'a when 'a :> IHasTimeCode> startTime count (items: 'a seq) =
+    items
+    |> Seq.filter (fun x -> x.Time >= startTime)
+    |> Seq.map (fun x -> x.Time)
+    // Notes on the same timecode (i.e. split chords) count as one
+    |> Seq.distinct
+    |> Seq.truncate count
 
-    let chordTimes =
-        level.Chords
-        |> Seq.filter (fun c -> c.Time >= startTime)
-        |> Seq.map (fun c -> c.Time)
-        |> Seq.distinct
-        |> Seq.truncate nthNote
+let internal findTimeOfNthNoteFrom (level: Level) (startTime: int) (nthNote: int) =
+    let noteTimes = level.Notes |> getTimes startTime nthNote
+    let chordTimes = level.Chords |> getTimes startTime nthNote
 
     noteTimes
     |> Seq.append chordTimes
