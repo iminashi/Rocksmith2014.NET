768a79e908f6774094a49355db9207c0411aa098
Tapio Malmberg <tapio.malmberg@gmail.com>
2020-12-28 17:43:45 +0200

    [dlcbuilder] Improve the config editor
    Add tabs for "general" and "build".

--- samples/DLCBuilder/Views/ConfigEditor.fs
+++ samples/DLCBuilder/Views/ConfigEditor.fs
@@ -1,32 +1,24 @@
 ﻿module DLCBuilder.ConfigEditor
 
+open Avalonia.FuncUI
 open Avalonia.FuncUI.DSL
-open Avalonia.FuncUI.Types
 open Avalonia.Controls
+open Avalonia.Controls.Shapes
 open Avalonia.Layout
+open Avalonia.Media
 open Rocksmith2014.Common
 
-let view state dispatch =
+let private generalConfig state dispatch =
     Grid.create [
-        Grid.columnDefinitions "*,*"
-        Grid.rowDefinitions "*,*,*,*,*,*,*,*,*,*,*"
+        Grid.columnDefinitions "*,2*"
+        Grid.rowDefinitions "*,*,*,*,*,*,*"
         Grid.children [
             TextBlock.create [
-                Grid.columnSpan 2
-                TextBlock.fontSize 16.
-                TextBlock.verticalAlignment VerticalAlignment.Center
-                TextBlock.horizontalAlignment HorizontalAlignment.Center
-                TextBlock.text (state.Localization.GetString "configuration")
-            ]
-            TextBlock.create [
-                Grid.row 1
                 TextBlock.verticalAlignment VerticalAlignment.Center
                 TextBlock.text (state.Localization.GetString "language")
             ]
             ComboBox.create [
                 Grid.column 1
-                Grid.row 1
-                ComboBox.margin 2.
                 ComboBox.dataItems [ Locales.English; Locales.Finnish ]
                 ComboBox.selectedItem state.Config.Locale
                 ComboBox.onSelectedItemChanged (fun item ->
@@ -34,14 +26,161 @@ let view state dispatch =
                     | :? Locale as l -> l |> ChangeLocale |> dispatch
                     | _ -> ())
             ]
+
+            TextBlock.create [
+                Grid.row 1
+                TextBlock.verticalAlignment VerticalAlignment.Center
+                TextBlock.text (state.Localization.GetString "charterName")
+            ]
+            TextBox.create [
+                Grid.column 1
+                Grid.row 1
+                TextBox.margin (0., 4.)
+                TextBox.text state.Config.CharterName
+                TextBox.onTextChanged (fun name ->
+                    fun c -> { c with CharterName = name }
+                    |> EditConfig
+                    |> dispatch)
+            ]
+
             TextBlock.create [
                 Grid.row 2
+                TextBlock.verticalAlignment VerticalAlignment.Center
+                TextBlock.text (state.Localization.GetString "profilePath")
+            ]
+
+            StackPanel.create [
+                Grid.column 1
+                Grid.row 2
+                StackPanel.orientation Orientation.Horizontal
+                StackPanel.children [
+                    TextBox.create [
+                        TextBox.margin (0., 4.)
+                        TextBox.width 250.
+                        TextBox.text state.Config.ProfilePath
+                        TextBox.onTextChanged (SetProfilePath >> dispatch)
+                    ]
+                    Button.create [
+                        Button.margin (0., 4.)
+                        Button.content "..."
+                        Button.onClick (fun _ -> dispatch (Msg.OpenFileDialog("selectProfile", Dialogs.profileFilter, SetProfilePath)))
+                    ]
+                ]
+            ]
+
+            TextBlock.create [
+                Grid.row 3
+                TextBlock.verticalAlignment VerticalAlignment.Center
+                TextBlock.text (state.Localization.GetString "testFolder")
+            ]
+
+            StackPanel.create [
+                Grid.column 1
+                Grid.row 3
+                StackPanel.orientation Orientation.Horizontal
+                StackPanel.children [
+                    TextBox.create [
+                        TextBox.margin (0., 4.)
+                        TextBox.width 250.
+                        TextBox.text state.Config.TestFolderPath
+                        TextBox.onTextChanged (SetTestFolderPath >> dispatch)
+                    ]
+                    Button.create [
+                        Button.margin (0., 4.)
+                        Button.content "..."
+                        Button.onClick (fun _ -> dispatch (Msg.OpenFolderDialog("selectTestFolder", SetTestFolderPath)))
+                    ]
+                ]
+            ]
+
+            TextBlock.create [
+                Grid.row 4
+                TextBlock.verticalAlignment VerticalAlignment.Center
+                TextBlock.text (state.Localization.GetString "projectsFolder")
+            ]
+            StackPanel.create [
+                Grid.column 1
+                Grid.row 4
+                StackPanel.orientation Orientation.Horizontal
+                StackPanel.children [
+                    TextBox.create [
+                        TextBox.margin (0., 4.)
+                        TextBox.width 250.
+                        TextBox.text state.Config.ProjectsFolderPath
+                        TextBox.onTextChanged (SetProjectsFolderPath >> dispatch)
+                    ]
+                    Button.create [
+                        Button.margin (0., 4.)
+                        Button.content "..."
+                        Button.onClick (fun _ -> dispatch (Msg.OpenFolderDialog("selectProjectFolder", SetProjectsFolderPath)))
+                    ]
+                ]
+            ]
+
+            TextBlock.create [
+                Grid.row 5
+                TextBlock.verticalAlignment VerticalAlignment.Center
+                TextBlock.text (state.Localization.GetString "wwiseConsolePath")
+            ]
+            StackPanel.create [
+                Grid.column 1
+                Grid.row 5
+                StackPanel.orientation Orientation.Horizontal
+                StackPanel.children [
+                    TextBox.create [
+                        TextBox.margin (0., 4.)
+                        TextBox.width 250.
+                        TextBox.text (Option.toObj state.Config.WwiseConsolePath)
+                        TextBox.onTextChanged (SetWwiseConsolePath >> dispatch)
+                        ToolTip.tip (state.Localization.GetString "wwiseConsolePathTooltip")
+                    ]
+                    Button.create [
+                        Button.margin (0., 4.)
+                        Button.content "..."
+                        Button.onClick (fun _ ->
+                            dispatch (Msg.OpenFileDialog("selectWwiseConsolePath",
+                                                         Dialogs.wwiseConsoleAppFilter state.CurrentPlatform,
+                                                         SetWwiseConsolePath))
+                        )
+                    ]
+                ]
+            ]
+
+            TextBlock.create [
+                Grid.row 6
+                TextBlock.margin (0., 0., 4., 0.)
+                TextBlock.verticalAlignment VerticalAlignment.Center
+                TextBlock.text (state.Localization.GetString "showAdvancedFeatures")
+            ]
+            CheckBox.create [
+                Grid.column 1
+                Grid.row 6
+                CheckBox.isChecked state.Config.ShowAdvanced
+                CheckBox.onChecked (fun _ ->
+                    fun c -> { c with ShowAdvanced = true }
+                    |> EditConfig
+                    |> dispatch)
+                CheckBox.onUnchecked (fun _ ->
+                    fun c -> { c with ShowAdvanced = false }
+                    |> EditConfig
+                    |> dispatch)
+            ]
+        ]
+     ]
+
+let private buildConfig state dispatch =
+    Grid.create [
+        Grid.columnDefinitions "*,2*"
+        Grid.rowDefinitions "*,*"
+        Grid.verticalAlignment VerticalAlignment.Top
+        Grid.children [
+            TextBlock.create [
                 TextBlock.verticalAlignment VerticalAlignment.Center
                 TextBlock.text (state.Localization.GetString "releasePlatforms")
             ]
             StackPanel.create [
                 Grid.column 1
-                Grid.row 2
+                StackPanel.margin (0., 2.)
                 StackPanel.orientation Orientation.Horizontal
                 StackPanel.children [
                     CheckBox.create [
@@ -76,29 +215,14 @@ let view state dispatch =
             ]
 
             TextBlock.create [
-                Grid.row 3
-                TextBlock.verticalAlignment VerticalAlignment.Center
-                TextBlock.text (state.Localization.GetString "charterName")
-            ]
-            TextBox.create [
-                Grid.column 1
-                Grid.row 3
-                TextBox.margin (0., 4.)
-                TextBox.text state.Config.CharterName
-                TextBox.onTextChanged (fun name ->
-                    fun c -> { c with CharterName = name }
-                    |> EditConfig
-                    |> dispatch)
-            ]
-
-            TextBlock.create [
-                Grid.row 4
+                Grid.row 1
                 TextBlock.verticalAlignment VerticalAlignment.Center
                 TextBlock.text (state.Localization.GetString "testingAppId")
             ]
             StackPanel.create [
                 Grid.column 1
-                Grid.row 4
+                Grid.row 1
+                StackPanel.margin (0., 2.)
                 StackPanel.children [
                     RadioButton.create [
                         RadioButton.content "Cherub Rock (248750)"
@@ -132,133 +256,39 @@ let view state dispatch =
                     ]
                 ]
             ]
+        ]
+     ]
 
-            TextBlock.create [
-                Grid.row 5
-                TextBlock.verticalAlignment VerticalAlignment.Center
-                TextBlock.text (state.Localization.GetString "profilePath")
-            ]
-
-            StackPanel.create [
-                Grid.column 1
-                Grid.row 5
-                StackPanel.orientation Orientation.Horizontal
-                StackPanel.children [
-                    TextBox.create [
-                        TextBox.margin (0., 4.)
-                        TextBox.width 200.
-                        TextBox.text state.Config.ProfilePath
-                        TextBox.onTextChanged (SetProfilePath >> dispatch)
-                    ]
-                    Button.create [
-                        Button.margin (0., 4.)
-                        Button.content "..."
-                        Button.onClick (fun _ -> dispatch (Msg.OpenFileDialog("selectProfile", Dialogs.profileFilter, SetProfilePath)))
-                    ]
-                ]
+let private tabHeader (icon: Geometry) text =
+    StackPanel.create [
+        StackPanel.minWidth 70.
+        StackPanel.children [
+            Path.create [
+                Path.fill Brushes.DarkGray
+                Path.data icon
+                Path.horizontalAlignment HorizontalAlignment.Center
             ]
 
             TextBlock.create [
-                Grid.row 6
-                TextBlock.verticalAlignment VerticalAlignment.Center
-                TextBlock.text (state.Localization.GetString "testFolder")
+                TextBlock.text text
+                TextBlock.horizontalAlignment HorizontalAlignment.Center
             ]
+        ]
+    ]
 
-            StackPanel.create [
-                Grid.column 1
-                Grid.row 6
-                StackPanel.orientation Orientation.Horizontal
-                StackPanel.children [
-                    TextBox.create [
-                        TextBox.margin (0., 4.)
-                        TextBox.width 200.
-                        TextBox.text state.Config.TestFolderPath
-                        TextBox.onTextChanged (SetTestFolderPath >> dispatch)
-                    ]
-                    Button.create [
-                        Button.margin (0., 4.)
-                        Button.content "..."
-                        Button.onClick (fun _ -> dispatch (Msg.OpenFolderDialog("selectTestFolder", SetTestFolderPath)))
-                    ]
-                ]
-            ]
-
+let view state dispatch =
+    DockPanel.create [
+        DockPanel.children [
             TextBlock.create [
-                Grid.row 7
+                DockPanel.dock Dock.Top
+                TextBlock.fontSize 16.
                 TextBlock.verticalAlignment VerticalAlignment.Center
-                TextBlock.text (state.Localization.GetString "projectsFolder")
-            ]
-            StackPanel.create [
-                Grid.column 1
-                Grid.row 7
-                StackPanel.orientation Orientation.Horizontal
-                StackPanel.children [
-                    TextBox.create [
-                        TextBox.margin (0., 4.)
-                        TextBox.width 200.
-                        TextBox.text state.Config.ProjectsFolderPath
-                        TextBox.onTextChanged (SetProjectsFolderPath >> dispatch)
-                    ]
-                    Button.create [
-                        Button.margin (0., 4.)
-                        Button.content "..."
-                        Button.onClick (fun _ -> dispatch (Msg.OpenFolderDialog("selectProjectFolder", SetProjectsFolderPath)))
-                    ]
-                ]
-            ]
-
-            TextBlock.create [
-                Grid.row 8
-                TextBlock.verticalAlignment VerticalAlignment.Center
-                TextBlock.text (state.Localization.GetString "wwiseConsolePath")
-            ]
-            StackPanel.create [
-                Grid.column 1
-                Grid.row 8
-                StackPanel.orientation Orientation.Horizontal
-                StackPanel.children [
-                    TextBox.create [
-                        TextBox.margin (0., 4.)
-                        TextBox.width 200.
-                        TextBox.text (Option.toObj state.Config.WwiseConsolePath)
-                        TextBox.onTextChanged (SetWwiseConsolePath >> dispatch)
-                        ToolTip.tip (state.Localization.GetString "wwiseConsolePathTooltip")
-                    ]
-                    Button.create [
-                        Button.margin (0., 4.)
-                        Button.content "..."
-                        Button.onClick (fun _ ->
-                            dispatch (Msg.OpenFileDialog("selectWwiseConsolePath",
-                                                         Dialogs.wwiseConsoleAppFilter state.CurrentPlatform,
-                                                         SetWwiseConsolePath))
-                        )
-                    ]
-                ]
-            ]
-
-            TextBlock.create [
-                Grid.row 9
-                TextBlock.margin (0., 0., 4., 0.)
-                TextBlock.verticalAlignment VerticalAlignment.Center
-                TextBlock.text (state.Localization.GetString "showAdvancedFeatures")
-            ]
-            CheckBox.create [
-                Grid.column 1
-                Grid.row 9
-                CheckBox.isChecked state.Config.ShowAdvanced
-                CheckBox.onChecked (fun _ ->
-                    fun c -> { c with ShowAdvanced = true }
-                    |> EditConfig
-                    |> dispatch)
-                CheckBox.onUnchecked (fun _ ->
-                    fun c -> { c with ShowAdvanced = false }
-                    |> EditConfig
-                    |> dispatch)
+                TextBlock.horizontalAlignment HorizontalAlignment.Center
+                TextBlock.text (state.Localization.GetString "configuration")
             ]
 
             Button.create [
-                Grid.columnSpan 2
-                Grid.row 10
+                DockPanel.dock Dock.Bottom
                 Button.margin 4.
                 Button.fontSize 16.
                 Button.padding (50., 10.)
@@ -266,5 +296,23 @@ let view state dispatch =
                 Button.content (state.Localization.GetString "close")
                 Button.onClick (fun _ -> CloseOverlay |> dispatch)
             ]
+
+            TabControl.create [
+                TabControl.minHeight 250.
+                TabControl.minWidth 520.
+                TabControl.tabStripPlacement Dock.Left
+                TabControl.viewItems [
+                    TabItem.create [
+                        TabItem.horizontalAlignment HorizontalAlignment.Center
+                        TabItem.header (tabHeader Media.Icons.cog (state.Localization.GetString "general"))
+                        TabItem.content (generalConfig state dispatch)
+                    ]
+                    TabItem.create [
+                        TabItem.horizontalAlignment HorizontalAlignment.Center
+                        TabItem.header (tabHeader Media.Icons.package (state.Localization.GetString "build"))
+                        TabItem.content (buildConfig state dispatch)
+                    ]
+                ]
+            ]
         ]
-    ] :> IView
+    ] |> Helpers.generalize
