83ee0ed77ed2a63943144dd46d4be172b4495b48
Tapio Malmberg <tapio.malmberg@gmail.com>
2021-01-31 17:01:42 +0200

    [audio] Replace loop with recursion
    Fix for ogg files that get stuck in an infinite loop for some reason.

--- src/Rocksmith2014.Audio/Volume/Volume.fs
+++ src/Rocksmith2014.Audio/Volume/Volume.fs
@@ -2,27 +2,32 @@
 
 open System
 open System.Buffers
+open NAudio.Wave
 
 let [<Literal>] private BufferSize = 50_000
 
+let private processData (sampleProvider: ISampleProvider) (lufsMeter: LufsMeter) =
+    let buffer = ArrayPool<float32>.Shared.Rent BufferSize
+    let channels = sampleProvider.WaveFormat.Channels
+
+    let rec loop () =
+        match sampleProvider.Read(buffer, 0, BufferSize) with
+        | 0 -> ()
+        | samplesRead ->
+            let perChannel = samplesRead / channels
+            Array.init channels (fun ch ->
+                Array.init perChannel (fun pos -> float buffer.[pos * channels + ch])
+            )
+            |> lufsMeter.ProcessBuffer
+            loop ()
+
+    try loop () finally ArrayPool.Shared.Return buffer
+
 /// Calculates a volume value using BS.1770 integrated loudness with -16 as reference value.
 let calculate (fileName: string) =
-    let buffer = ArrayPool<float32>.Shared.Rent(BufferSize)
-
-    try
-        use audio = AudioReader.Create fileName
-        let sampleProvider = audio.SampleProvider
-        let lufsMeter = LufsMeter(float sampleProvider.WaveFormat.SampleRate, sampleProvider.WaveFormat.Channels)
-        let channels = sampleProvider.WaveFormat.Channels
-
-        while audio.Position < audio.Length do
-            let samplesRead = sampleProvider.Read(buffer, 0, BufferSize)
-            if samplesRead > 0 then
-                let perChannel = samplesRead / channels
-                Array.init channels (fun ch ->
-                    Array.init perChannel (fun pos -> float buffer.[pos * channels + ch])
-                )
-                |> lufsMeter.ProcessBuffer
-
-        Math.Round(-1. * (16. + lufsMeter.GetIntegratedLoudness()), 1)
-    finally ArrayPool.Shared.Return buffer
+    use audio = AudioReader.Create fileName
+    let sampleProvider = audio.SampleProvider
+    let lufsMeter = LufsMeter(float sampleProvider.WaveFormat.SampleRate, sampleProvider.WaveFormat.Channels)
+    processData sampleProvider lufsMeter
+    
+    Math.Round(-1. * (16. + lufsMeter.GetIntegratedLoudness()), 1)
