742361ff8873d5c51c08a0e01ede25d085d3c0af
Tapio Malmberg <tapio.malmberg@gmail.com>
2021-02-15 15:55:23 +0200

    Add tests for tone import/export

--- tests/Rocksmith2014.Common.Tests/ToneTests.fs
+++ tests/Rocksmith2014.Common.Tests/ToneTests.fs
@@ -2,6 +2,8 @@
 
 open Expecto
 open Rocksmith2014.Common.Manifest
+open System.IO
+open System
 
 [<Tests>]
 let toneTests =
@@ -10,6 +12,42 @@ let toneTests =
             let tone = Tone.fromXmlFile "test.tone2014.xml"
 
             Expect.equal tone.Volume "-12" "Volume is correct"
+            Expect.equal tone.Key "Test" "Key is correct"
+            Expect.equal tone.Name "Test" "Name is correct"
+            Expect.equal tone.ToneDescriptors [| "$[35720]CLEAN" |] "Descriptors are correct"
+            Expect.equal tone.GearList.Cabinet.Category "Dynamic_Cone" "Cabinet category is correct"
+            Expect.equal tone.GearList.Cabinet.Skin null "Cabinet skin is null"
+            Expect.equal tone.GearList.Amp.Key "Amp_OrangeAD50" "Amp key is correct"
+            Expect.hasLength tone.GearList.Amp.KnobValues 4 "Amp knob values length is correct" }
+
+        testAsync "Can be exported to XML" {
+            let testFile = "testExport.tone2014.xml"
+            if File.Exists testFile then File.Delete testFile
+            let imported = Tone.fromXmlFile "test.tone2014.xml"
+
+            do! Tone.exportXml testFile imported
+            let tone = Tone.fromXmlFile testFile
+
+            Expect.equal tone.Volume "-12" "Volume is correct"
+            Expect.equal tone.Key "Test" "Key is correct"
+            Expect.equal tone.Name "Test" "Name is correct"
+            Expect.equal tone.ToneDescriptors [| "$[35720]CLEAN" |] "Descriptors are correct"
+            Expect.equal tone.GearList.Cabinet.Type "Cabinets" "Cabinet type is correct"
+            Expect.equal tone.GearList.Cabinet.SkinIndex (Nullable()) "Cabinet skin index is null"
+            Expect.equal tone.GearList.Amp.Type "Amps" "Amp type is correct"
+            Expect.hasLength tone.GearList.Amp.KnobValues 4 "Amp knob values length is correct" }
+
+        testAsync "Can be exported to JSON and imported from JSON" {
+            let testFile = "testExport.tone2014.json"
+            if File.Exists testFile then File.Delete testFile
+            let imported = Tone.fromXmlFile "test.tone2014.xml"
+
+            do! Tone.exportJson testFile imported
+            let! tone = Tone.fromJsonFile testFile
+
+            Expect.equal tone.Volume "-12" "Volume is correct"
+            Expect.equal tone.Key "Test" "Key is correct"
+            Expect.equal tone.Name "Test" "Name is correct"
             Expect.equal tone.ToneDescriptors [| "$[35720]CLEAN" |] "Descriptors are correct"
             Expect.equal tone.GearList.Cabinet.Category "Dynamic_Cone" "Cabinet category is correct"
             Expect.equal tone.GearList.Cabinet.Skin null "Cabinet skin is null"
