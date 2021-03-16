1130d2b823aff8d478e8d817e952ef479e23a432
Tapio Malmberg <tapio.malmberg@gmail.com>
2021-03-16 21:58:03 +0200

    Refactor wem conversion tests

--- tests/Rocksmith2014.Audio.Tests/WwiseTests.fs
+++ tests/Rocksmith2014.Audio.Tests/WwiseTests.fs
@@ -4,26 +4,19 @@ open Expecto
 open Rocksmith2014.Audio
 open System.IO
 
-let wemPath = Path.ChangeExtension(TestFiles.WaveFile, "wem")
+let testConversion testFile = async {
+    let wemPath = Path.ChangeExtension(testFile, "wem")
+    if File.Exists wemPath then File.Delete wemPath
+
+    do! Wwise.convertToWem None testFile
+    let info = FileInfo(wemPath)          
+
+    Expect.isTrue info.Exists "Wem file was created"
+    Expect.isGreaterThan info.Length 100_000L "File is larger than 100KB" }
 
 [<Tests>]
 let wwiseTests =
     testList "Wwise Conversion Tests" [
-        testAsync "Wave file can be converted" {
-            if File.Exists wemPath then File.Delete wemPath
-
-            do! Wwise.convertToWem None TestFiles.WaveFile
-            let info = FileInfo(wemPath)          
-
-            Expect.isTrue info.Exists "Wem file was created"
-            Expect.isGreaterThan info.Length 100_000L "File is larger than 100KB" }
-
-        testAsync "Vorbis file can be converted" {
-            if File.Exists wemPath then File.Delete wemPath
-
-            do! Wwise.convertToWem None TestFiles.VorbisFile
-            let info = FileInfo(wemPath)
-
-            Expect.isTrue info.Exists "Wem file was created"
-            Expect.isGreaterThan info.Length 100_000L "File is larger than 100KB" }
+        testAsync "Wave file can be converted" { do! testConversion TestFiles.WaveFile }
+        testAsync "Vorbis file can be converted" { do! testConversion TestFiles.VorbisFile }
     ]
