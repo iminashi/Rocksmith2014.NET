dc16c99c7f4456fc0a6248f92dc341f26ba6b993
Tapio Malmberg <tapio.malmberg@gmail.com>
2021-03-21 22:29:08 +0200

    Add support for Wwise v2021

--- src/Rocksmith2014.Audio/Wwise/Wwise.fs
+++ src/Rocksmith2014.Audio/Wwise/Wwise.fs
@@ -1,20 +1,23 @@
 ﻿module Rocksmith2014.Audio.Wwise
 
 open System
+open System.Diagnostics
 open System.IO
 open System.IO.Compression
 open System.Reflection
-open System.Diagnostics
+open System.Text.RegularExpressions
 open Microsoft.Extensions.FileProviders
 open Rocksmith2014.Common
 open Rocksmith2014.Common.BinaryWriters
+
+type private WwiseVersion = Wwise2019 | Wwise2021
 
 let private tryFindWwiseInstallation rootDir =
     match Path.Combine(rootDir, "Audiokinetic") with
     | dir when Directory.Exists dir ->
         dir
         |> Directory.EnumerateDirectories
-        |> Seq.tryFind (String.contains "2019")
+        |> Seq.tryFind (fun fn -> Regex.IsMatch(fn, "20(19|21)"))
     | _ ->
         None
 
@@ -24,19 +27,19 @@ let private getCLIPath () =
         if OperatingSystem.IsWindows() then
             let wwiseRoot =
                 match Environment.GetEnvironmentVariable "WWISEROOT" |> Option.ofString with
-                | Some (Contains "2019" as path) ->
+                | Some (Contains "2019" | Contains "2021" as path) ->
                     path
                 | _ ->
                     // Try the default installation directory in program files
                     tryFindWwiseInstallation (Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86))
                     |> Option.orElseWith (fun () -> tryFindWwiseInstallation (Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles)))
-                    |> Option.defaultWith (fun () -> failwith "Could not locate Wwise 2019 installation from WWISEROOT environment variable or path Program Files\Audiokinetic.")
+                    |> Option.defaultWith (fun () -> failwith "Could not locate Wwise 2019 or 2021 installation from WWISEROOT environment variable or path Program Files\Audiokinetic.")
 
             Path.Combine(wwiseRoot, @"Authoring\x64\Release\bin\WwiseConsole.exe")
         elif OperatingSystem.IsMacOS() then
             let wwiseAppPath =
                 tryFindWwiseInstallation "/Applications"
-                |> Option.defaultWith (fun () -> failwith "Could not find Wwise 2019 installation in /Applications/Audiokinetic/")
+                |> Option.defaultWith (fun () -> failwith "Could not find Wwise 2019 or 2021 installation in /Applications/Audiokinetic/")
 
             Path.Combine(wwiseAppPath, "Wwise.app/Contents/Tools/WwiseConsole.sh")
         else
@@ -54,16 +57,17 @@ let private getTempDirectory () =
     (Directory.CreateDirectory dir).FullName
 
 /// Extracts the Wwise template into the target directory.
-let private extractTemplate targetDir =
+let private extractTemplate targetDir version =
     let embeddedProvider = EmbeddedFileProvider(Assembly.GetExecutingAssembly())
-    use templateZip = embeddedProvider.GetFileInfo("Wwise/wwise2019.zip").CreateReadStream()
+    let templateFile = version.ToString().ToLowerInvariant()
+    use templateZip = embeddedProvider.GetFileInfo($"Wwise/{templateFile}.zip").CreateReadStream()
     using (new ZipArchive(templateZip)) (fun zip -> zip.ExtractToDirectory targetDir)
 
 /// Extracts the Wwise template and copies the audio files into the Originals/SFX directory.
-let private loadTemplate sourcePath =
+let private loadTemplate sourcePath version =
     let templateDir = getTempDirectory()
     let targetPath = Path.Combine(templateDir, "Originals", "SFX", "Audio.wav")
-    extractTemplate templateDir
+    extractTemplate templateDir version
     
     match sourcePath with
     | EndsWith ".wav" -> File.Copy(sourcePath, targetPath, overwrite=true)
@@ -90,23 +94,30 @@ let private copyWemFile (destPath: string) (templateDir: string) =
     File.Copy(wemFiles.[0], destPath, overwrite=true)
     fixHeader destPath
 
+let private getWwiseVersion = function
+    | Contains "2019" -> Wwise2019
+    | Contains "2021" -> Wwise2021
+    | _ -> Wwise2019
+
 /// Converts the source audio file into a wem file.
 let convertToWem (cliPath: string option) (sourcePath: string) = async {
     let destPath = Path.ChangeExtension(sourcePath, "wem")
     let cliPath = cliPath |> Option.defaultWith getCLIPath
-    let templateDir = loadTemplate sourcePath
-    
-    let args =
-        Path.Combine(templateDir, "Template.wproj")
-        |> sprintf """generate-soundbank "%s" --platform "Windows" --language "English(US)" --no-decode --quiet"""
-    
-    let startInfo = ProcessStartInfo(FileName = cliPath, Arguments = args, CreateNoWindow = true, RedirectStandardOutput = true)
-    use wwiseCli = new Process(StartInfo = startInfo)
-    wwiseCli.Start() |> ignore
-    do! wwiseCli.WaitForExitAsync()
+    let version = getWwiseVersion cliPath
+    let templateDir = loadTemplate sourcePath version
 
-    let output = wwiseCli.StandardOutput.ReadToEnd()
-    if output.Length > 0 then failwith output
+    try
+        let args =
+            Path.Combine(templateDir, "Template.wproj")
+            |> sprintf """generate-soundbank "%s" --platform "Windows" --language "English(US)" --no-decode --quiet"""
     
-    copyWemFile destPath templateDir
-    Directory.Delete(templateDir, true) }
+        let startInfo = ProcessStartInfo(FileName = cliPath, Arguments = args, CreateNoWindow = true, RedirectStandardOutput = true)
+        use wwiseCli = new Process(StartInfo = startInfo)
+        wwiseCli.Start() |> ignore
+        do! wwiseCli.WaitForExitAsync()
+
+        let output = wwiseCli.StandardOutput.ReadToEnd()
+        if output.Length > 0 then failwith output
+    
+        copyWemFile destPath templateDir
+    finally Directory.Delete(templateDir, true) }
