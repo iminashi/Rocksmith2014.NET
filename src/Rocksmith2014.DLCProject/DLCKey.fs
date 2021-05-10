b6de68e8af5bb96222965124aab3489b1fe0dd6a
Tapio Malmberg <tapio.malmberg@gmail.com>
2021-05-10 22:58:52 +0300

    Clean up DLCKey module

--- src/Rocksmith2014.DLCProject/DLCKey.fs
+++ src/Rocksmith2014.DLCProject/DLCKey.fs
@@ -1,30 +1,29 @@
-﻿namespace Rocksmith2014.DLCProject
+﻿module Rocksmith2014.DLCProject.DLCKey
 
 open System
 open Rocksmith2014.Common
 
-module DLCKey =
-    let [<Literal>] MinimumLength = 5
+let [<Literal>] MinimumLength = 5
 
-    let private createPart str =
-        let p = StringValidator.dlcKey str
-        p.Substring(0, min MinimumLength p.Length)
+let private createPart str =
+    let p = StringValidator.dlcKey str
+    p.Substring(0, min 5 p.Length)
 
-    let private randomChars count =
-        Array.init count (fun _ -> RandomGenerator.nextAlphabet())
-        |> String
+let private randomChars count =
+    Array.init count (fun _ -> RandomGenerator.nextAlphabet())
+    |> String
 
-    let private createPrefix charterName =
-        let name = StringValidator.dlcKey charterName
-        if name.Length < 2 then
-            randomChars 2
-        else
-            name.Substring(0, 2)
+let private createPrefix charterName =
+    let name = StringValidator.dlcKey charterName
+    if name.Length < 2 then
+        randomChars 2
+    else
+        name.Substring(0, 2)
 
-    /// Creates a DLC key from the charter name, artist name and title.
-    let create (charterName: string) (artist: string) (title: string) =
-        let key = $"{createPrefix charterName}{createPart artist}{createPart title}"
-        if key.Length < MinimumLength then
-            key + randomChars (MinimumLength - key.Length)
-        else
-            key
+/// Creates a DLC key from the charter name, artist name and title.
+let create (charterName: string) (artist: string) (title: string) =
+    let key = $"{createPrefix charterName}{createPart artist}{createPart title}"
+    if key.Length < MinimumLength then
+        key + randomChars (MinimumLength - key.Length)
+    else
+        key
