aa52af3f26de1424f63be114f174a011eaaea2d1
Tapio Malmberg <tapio.malmberg@gmail.com>
2021-05-27 01:24:19 +0300

    Improve the publish script
    -Create an app bundle for the macOS version.
    -Replace Path.Combine with FAKE operator.

--- publish.fsx
+++ publish.fsx
@@ -8,64 +8,109 @@ open Fake.Api
 open Fake.Core
 open Fake.DotNet
 open Fake.IO
+open Fake.IO.FileSystemOperators
 open Fake.IO.Globbing.Operators
 open System
 open System.IO
+open System.Runtime.InteropServices
+
+type TargetPlatForm =
+    | Windows
+    | MacOS
+    override this.ToString() =
+        match this with
+        | Windows -> "win"
+        | MacOS -> "mac"
 
 let gitOwner = "iminashi"
 let gitName = "Rocksmith2014.NET"
 
-let publishDir = Path.Combine(__SOURCE_DIRECTORY__, "publish")
-let samplesDir = Path.Combine(__SOURCE_DIRECTORY__, "samples")
-let dlcBuilderProject = Path.Combine(samplesDir, "DLCBuilder", "DLCBuilder.fsproj")
-let msBuildParams = { MSBuild.CliArguments.Create() with Properties = [ "PublishSingleFile", "true" ] }
+let publishDir = __SOURCE_DIRECTORY__ </> "publish"
+let samplesDir = __SOURCE_DIRECTORY__ </> "samples"
+let dlcBuilderDir = samplesDir </> "DLCBuilder"
+let msBuildParams =
+    { MSBuild.CliArguments.Create() with Properties = [ "PublishSingleFile", "true" ] }
+
+let release =
+    Path.Combine(dlcBuilderDir, "RELEASE_NOTES.md")
+    |> ReleaseNotes.load
 
 let cleanPublishDirectory () = Directory.Delete(publishDir, recursive = true)
 
-let createConfig runTime directory (arg: DotNet.PublishOptions) =
-    let targetDir = Path.Combine(publishDir, directory)
+let createConfig appName platform (arg: DotNet.PublishOptions) =
+    let targetDir = publishDir </> $"{appName}-{platform}"
+    let runTime, isMac =
+        match platform with
+        | Windows -> "win-x64", false
+        | MacOS -> "osx-x64", true
 
     { arg with OutputPath = Some targetDir
                Configuration = DotNet.BuildConfiguration.Release
                Runtime = Some runTime
-               SelfContained = Some (runTime = "osx-x64")
+               SelfContained = Some isMac
                MSBuildParams = msBuildParams }
 
-let publishUpdaterWin () =
-    let project = Path.Combine(samplesDir, "Updater", "Updater.fsproj")
+let makeExecutable file =
+    CreateProcess.fromRawCommand "chmod" [ "+x"; file ]
+    |> Proc.run
+    |> ignore
 
-    DotNet.publish (createConfig "win-x64" "updater-win") project
+let publishUpdater platform =
+    samplesDir </> "Updater" </> "Updater.fsproj"
+    |> DotNet.publish (createConfig "updater" platform)
 
-let publishBuilderWin () =
-    let targetDir = Path.Combine(publishDir, "dlcbuilder-win")
+let createMacAppBundle buildDir =
+    if not <| RuntimeInformation.IsOSPlatform(OSPlatform.Windows) then
+        Shell.cd buildDir
+        makeExecutable "DLCBuilder"
+        makeExecutable "./Tools/ww2ogg"
+        makeExecutable "./Tools/revorb"
 
-    DotNet.publish (createConfig "win-x64" "dlcbuilder-win") dlcBuilderProject
+    // Create the app bundle directory structure and copy the build contents
+    let contentsDir = publishDir </> "DLC Builder.app" </> "Contents"
+    let resourceDir = contentsDir </> "Resources"
+    Directory.CreateDirectory(contentsDir) |> ignore
+    Directory.Move(buildDir, contentsDir </> "MacOS")
+    Directory.CreateDirectory(resourceDir) |> ignore
 
-    // Copy the updater
-    Directory.CreateDirectory(Path.Combine(targetDir, "Updater")) |> ignore
-    File.Copy(Path.Combine(publishDir, "updater-win", "Updater.exe"),
-              Path.Combine(targetDir, "Updater", "Updater.exe"), overwrite = true)
+    // Copy the icon
+    let macSourceDir = dlcBuilderDir </> "macOS"
+    File.Copy(macSourceDir </> "icon.icns", resourceDir </> "icon.icns")
 
-let publishBuilderMac () =
-    let targetDir = Path.Combine(publishDir, "dlcbuilder-mac")
+    // Create Info.plist with the program version set
+    File.readAsString (macSourceDir </> "Info.plist")
+    |> String.replace "%VERSION%" release.NugetVersion
+    |> File.writeString false (contentsDir </> "Info.plist")
 
-    DotNet.publish (createConfig "osx-x64" "dlcbuilder-mac") dlcBuilderProject
+let publishBuilder platform =
+    dlcBuilderDir </> "DLCBuilder.fsproj"
+    |> DotNet.publish (createConfig "dlcbuilder" platform)
 
-    // Copy the (temporary) setup instructions
-    File.Copy(Path.Combine(samplesDir, "DLCBuilder", "MacSetup.txt"),
-              Path.Combine(targetDir, "MacSetup.txt"), overwrite = true)
-
-let release =
-    Path.Combine(samplesDir, "DLCBuilder", "RELEASE_NOTES.md")
-    |> ReleaseNotes.load
+    let targetDir = publishDir </> $"dlcbuilder-{platform}"
+    match platform with
+    | Windows ->
+        // Copy the updater
+        Directory.CreateDirectory(targetDir </> "Updater") |> ignore
+        File.Copy(publishDir </> "updater-win" </> "Updater.exe",
+                  targetDir </> "Updater" </> "Updater.exe", overwrite = true)
+    | MacOS ->
+        createMacAppBundle targetDir
 
 let createZipArchives () =
-    [ "win"; "mac" ]
+    [ Windows; MacOS ]
     |> List.map (fun platform ->
-        let targetFile = Path.Combine(publishDir, $"DLCBuilder-{platform}-{release.NugetVersion}.zip")
-        let dir = Path.Combine(publishDir, $"dlcbuilder-{platform}")
-        !! (sprintf "%s/**" dir)
-        |> Zip.zip dir targetFile
+        let targetFile = publishDir </> $"DLCBuilder-{platform}-{release.NugetVersion}.zip"
+        let workingDir, dirToZip =
+            match platform with
+            | Windows ->
+                let dir = publishDir </> "dlcbuilder-win"
+                dir, dir
+            | MacOS ->
+                publishDir,
+                publishDir </> "DLC Builder.app"
+
+        !! $"{dirToZip}/**" |> Zip.zip workingDir targetFile
+
         targetFile)
 
 let createGitHubRelease files =
@@ -89,9 +134,9 @@ let createGitHubRelease files =
     |> Async.RunSynchronously
 
 cleanPublishDirectory()
-publishUpdaterWin()
-publishBuilderWin()
-publishBuilderMac()
+publishUpdater Windows
+publishBuilder Windows
+publishBuilder MacOS
 
 createZipArchives()
 |> createGitHubRelease
