826cbadb854c3dbc075b1572e28443bb21448531
Tapio Malmberg <tapio.malmberg@gmail.com>
2021-05-28 14:12:44 +0300

    Update unit tests
    -Update Microsoft.NET.Test.Sdk
    -Add coverlet.collector
    -Add tests for audio conversion

--- tests/Rocksmith2014.Audio.Tests/ConversionTests.fs
+++ tests/Rocksmith2014.Audio.Tests/ConversionTests.fs
@@ -4,17 +4,31 @@ open Expecto
 open Rocksmith2014.Audio
 open System.IO
 
-let targetFile = "convtest.wav"
-
 [<Tests>]
 let conversionTests =
     testList "Audio Conversion Tests" [
         testCase "Vorbis file can be converted to wave file" <| fun _ ->
+            let targetFile = "convtest.wav"
             if File.Exists targetFile then File.Delete targetFile
             let oggLength = Utils.getLength TestFiles.VorbisFile
 
             Conversion.oggToWav TestFiles.VorbisFile targetFile
             let wavLength = Utils.getLength targetFile
 
-            Expect.equal wavLength oggLength  "Converted file is same length as the original file"
+            Expect.equal wavLength oggLength "Converted file is same length as the original file"
+
+        testCase "Wem file can be converted to vorbis file" <| fun _ ->
+            let targetFile = Path.ChangeExtension(TestFiles.WemFile, "ogg")
+            if File.Exists targetFile then File.Delete targetFile
+
+            Conversion.wemToOgg <| Path.GetFullPath(TestFiles.WemFile)
+            let oggLength = Utils.getLength targetFile
+
+            Expect.equal 42 oggLength.Seconds "Converted file is same length as the original file"
+
+        testCase "Conversion throws exception on error" <| fun _ ->
+            let wemFile = "nosuchfile.wem"
+
+            Expect.throwsC (fun () -> Conversion.wemToOgg wemFile)
+                           (fun ex -> Expect.stringContains ex.Message "Process failed with output" "Exception contains correct message")
     ]
--- tests/Rocksmith2014.Audio.Tests/TestFiles.fs
+++ tests/Rocksmith2014.Audio.Tests/TestFiles.fs
@@ -2,3 +2,4 @@
 
 let [<Literal>] WaveFile = "BWV0573_wave.wav"
 let [<Literal>] VorbisFile = "BWV0573_vorbis.ogg"
+let [<Literal>] WemFile = "BWV0573_wwise.wem"
