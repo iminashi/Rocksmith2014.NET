601c832a7db3c3d8b9f02cd89f1c5e888cf7ead2
Tapio Malmberg <tapio.malmberg@gmail.com>
2021-07-16 18:59:56 +0300

    Update Avalonia.FuncUI.DSL

--- samples/DLCBuilder/Controls/FuncUIExtensions.fs
+++ samples/DLCBuilder/Controls/FuncUIExtensions.fs
@@ -9,10 +9,6 @@ open Avalonia.FuncUI.Builder
 open Avalonia.Input
 open Avalonia.Media
 
-type MenuItem with
-    static member inputGesture<'t when 't :> MenuItem>(value: KeyGesture) : IAttr<'t> =
-        AttrBuilder<'t>.CreateProperty<KeyGesture>(MenuItem.InputGestureProperty, value, ValueNone)
-
 type ListBox with
     static member selection<'i, 't when 't :> ListBox>(value: SelectionModel<'i>) : IAttr<'t> =
         AttrBuilder<'t>.CreateProperty<SelectionModel<'i>>(ListBox.SelectionProperty, value, ValueNone)
