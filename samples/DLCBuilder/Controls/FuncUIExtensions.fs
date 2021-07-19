07874a1583d3fa97e9de0bddc1d06bd280cb0bdc
Tapio Malmberg <tapio.malmberg@gmail.com>
2021-07-19 19:04:28 +0300

    Revert Avalonia.FuncUI.DSL to latest version on nuget.org

--- samples/DLCBuilder/Controls/FuncUIExtensions.fs
+++ samples/DLCBuilder/Controls/FuncUIExtensions.fs
@@ -1,4 +1,4 @@
-﻿// fsharplint:disable MemberNames
+// fsharplint:disable MemberNames
 [<AutoOpen>]
 module FuncUIExtensions
 
@@ -8,6 +8,10 @@ open Avalonia.FuncUI.Types
 open Avalonia.FuncUI.Builder
 open Avalonia.Input
 open Avalonia.Media
+
+type MenuItem with
+    static member inputGesture<'t when 't :> MenuItem>(value: KeyGesture) : IAttr<'t> =
+        AttrBuilder<'t>.CreateProperty<KeyGesture>(MenuItem.InputGestureProperty, value, ValueNone)
 
 type ListBox with
     static member selection<'i, 't when 't :> ListBox>(value: SelectionModel<'i>) : IAttr<'t> =
