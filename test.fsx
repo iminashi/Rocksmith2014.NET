09386f096cb4f3dfdaccae9ecd22cb2dc45db62a
Tapio Malmberg <tapio.malmberg@gmail.com>
2021-08-12 09:01:12 +0300

    Update test script

--- test.fsx
+++ test.fsx
@@ -3,17 +3,35 @@
 // Fix: Unsupported log file format. Latest supported version is 9, the log file has version 13.
 #r "nuget: MSBuild.StructuredLogger"
 
+open System
 open Fake.DotNet
 open Fake.IO.Globbing.Operators
 
 DotNet.restore id "Rocksmith2014.sln"
 
-let setOptions (o: DotNet.TestOptions) =
-    { o with NoRestore = true
-             Configuration = DotNet.BuildConfiguration.Release }
+let runtime =
+    if OperatingSystem.IsMacOS() then
+        "osx-x64"
+    elif OperatingSystem.IsLinux() then
+        "linux-x64"
+    else
+        "win-x64"
+
+let runTest (projectPath: string) =
+    let rid =
+        if projectPath.EndsWith "Audio.Tests.fsproj" ||
+           projectPath.EndsWith "DLCProject.Tests.fsproj"
+        then
+            $" --runtime {runtime}"
+        else
+            String.Empty
+
+    let result = DotNet.exec id "test" $"\"{projectPath}\" --no-restore -c Release{rid}"
+    if result.ExitCode <> 0 then
+        failwith "Test failed."
 
 !! "tests/**/*.fsproj"
-|> Seq.iter (DotNet.test setOptions)
+|> Seq.iter runTest
 
 !! "tests/**/*.csproj"
-|> Seq.iter (DotNet.test setOptions)
+|> Seq.iter runTest
