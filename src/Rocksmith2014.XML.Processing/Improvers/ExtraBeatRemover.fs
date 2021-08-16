9f1db4d85f4e2e9cf57991e891073a0d9a8c6739
Tapio Malmberg <tapio.malmberg@gmail.com>
2021-08-16 23:20:24 +0300

    Use RemoveAt instead of Remove

--- src/Rocksmith2014.XML.Processing/Improvers/ExtraBeatRemover.fs
+++ src/Rocksmith2014.XML.Processing/Improvers/ExtraBeatRemover.fs
@@ -2,6 +2,8 @@
 
 open Rocksmith2014.XML
 
+let private removeLast (beats: ResizeArray<_>) = beats.RemoveAt(beats.Count - 1)
+
 /// Removes beats that come after the audio has ended.
 let improve (arrangement: InstrumentalArrangement) =
     let mutable lastBeat = arrangement.Ebeats.[arrangement.Ebeats.Count - 1]
@@ -9,14 +11,14 @@ let improve (arrangement: InstrumentalArrangement) =
     let audioEnd = arrangement.MetaData.SongLength
 
     while penultimateBeat.Time > audioEnd do
-        arrangement.Ebeats.Remove lastBeat |> ignore
+        removeLast arrangement.Ebeats
 
         lastBeat <- penultimateBeat
         penultimateBeat <- arrangement.Ebeats.[arrangement.Ebeats.Count - 2]
 
     // Remove the last beat unless it is closer to the audio end than the penultimate beat
     if audioEnd - penultimateBeat.Time <= lastBeat.Time - audioEnd then
-        arrangement.Ebeats.Remove lastBeat |> ignore
+        removeLast arrangement.Ebeats
         lastBeat <- penultimateBeat
 
     // Move the last beat to the time audio ends
