3229c685d5c9eba5f05a36bd1aaa7cf50d239dcb
Tapio Malmberg <tapio.malmberg@gmail.com>
2021-08-20 22:04:19 +0300

    Add a HelpButton control

---
+++ samples/DLCBuilder/Controls/HelpButton.fs
@@ -0,0 +1,36 @@
+// fsharplint:disable MemberNames
+namespace DLCBuilder
+
+open Avalonia.Controls
+open Avalonia.Controls.Shapes
+open Avalonia.FuncUI.Builder
+open Avalonia.FuncUI.Types
+open Avalonia.Media
+
+type HelpButton() =
+    inherit UserControl()
+
+    let text = TextBlock(TextWrapping = TextWrapping.Wrap, MaxWidth = 400., FontSize = 16.)
+    let flyOut = Flyout(ShowMode = FlyoutShowMode.TransientWithDismissOnPointerMoveAway, Content = text)
+    let icon = Path(Data = Media.Icons.helpOutline, Fill = Brushes.Gray)
+    let button = Button(Content = icon, Flyout = flyOut)
+
+    do
+        button.Classes.Add("icon-btn")
+        base.Content <- button
+
+    member _.HelpText
+        with get() = text.Text
+        and set(v) = text.Text <- v
+
+    static member helpText(text: string) =
+        let getter (c: HelpButton) = c.HelpText
+        let setter : HelpButton * string -> unit = fun (c, v) -> c.HelpText <- v
+
+        AttrBuilder<HelpButton>.CreateProperty<string>
+            ("SelectedIndex", text, ValueSome getter, ValueSome setter, ValueNone)
+
+[<RequireQualifiedAccess>]
+module HelpButton =
+    let create (attrs: IAttr<HelpButton> list): IView<HelpButton> =
+        ViewBuilder.Create<HelpButton>(attrs)
