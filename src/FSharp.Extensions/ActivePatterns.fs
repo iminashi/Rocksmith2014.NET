ed3f62847544ed184d939febfd0acbc3b45cad55
Tapio Malmberg <tapio.malmberg@gmail.com>
2021-09-15 11:53:17 +0300

    Add StartsWith active pattern

--- src/FSharp.Extensions/ActivePatterns.fs
+++ src/FSharp.Extensions/ActivePatterns.fs
@@ -9,6 +9,12 @@ let (|Contains|_|) (substr: string) (str: string) =
     else
         None
 
+let (|StartsWith|_|) (prefix: string) (str: string) =
+    if String.startsWith prefix str then
+        Some ()
+    else
+        None
+
 let (|EndsWith|_|) (suffix: string) (str: string) =
     if String.endsWith suffix str then
         Some ()
