93f649d41777f8e8f7d8de5a77beccbe55ce7211
Tapio Malmberg <tapio.malmberg@gmail.com>
2021-11-08 23:25:53 +0200

    Use ZLibStream instead of SharpZipLib

--- src/Rocksmith2014.Common/Compression.fs
+++ src/Rocksmith2014.Common/Compression.fs
@@ -2,31 +2,30 @@ module Rocksmith2014.Common.Compression
 
 open System
 open System.IO
-open ICSharpCode.SharpZipLib.Zip.Compression.Streams
-open ICSharpCode.SharpZipLib.Zip.Compression
+open System.IO.Compression
 
 /// Zips the data from the input stream into the output stream in zlib format, asynchronously, using the best compression.
 let asyncZip (input: Stream) (output: Stream) = async {
-    use zipStream = new DeflaterOutputStream(output, Deflater(Deflater.BEST_COMPRESSION), IsStreamOwner = false)
+    use zipStream = new ZLibStream(output, CompressionLevel.SmallestSize, leaveOpen=true)
     do! input.CopyToAsync(zipStream, 65536) }
 
 /// Zips the data from the input stream into the output stream in zlib format, using the best compression.
 let zip bufferSize (input: Stream) (output: Stream) =
-    use zipStream = new DeflaterOutputStream(output, Deflater(Deflater.BEST_COMPRESSION), IsStreamOwner = false)
+    use zipStream = new ZLibStream(output, CompressionLevel.SmallestSize, leaveOpen=true)
     input.CopyTo(zipStream, bufferSize)
 
 /// Unzips zlib data from the input stream into the output stream.
 let unzip (input: Stream) (output: Stream) =
-    use inflateStream = new InflaterInputStream(input, IsStreamOwner = false)
+    use inflateStream = new ZLibStream(input, CompressionMode.Decompress, leaveOpen=true)
     inflateStream.CopyTo(output, 65536)
 
 /// Asynchronously unzips zlib data from the input stream into the output stream.
 let asyncUnzip (input: Stream) (output: Stream) = async {
-    use inflateStream = new InflaterInputStream(input, IsStreamOwner = false)
+    use inflateStream = new ZLibStream(input, CompressionMode.Decompress, leaveOpen=true)
     do! inflateStream.CopyToAsync(output, 65536) }
 
 /// Returns an inflate stream for the input stream.
-let getInflateStream (input: Stream) = new InflaterInputStream(input, IsStreamOwner = false)
+let getInflateStream (input: Stream) = new ZLibStream(input, CompressionMode.Decompress, leaveOpen=true)
 
 /// Divides the input stream into zipped blocks with the maximum block size.
 let blockZip blockSize (deflatedData: ResizeArray<Stream>) (zLengths: ResizeArray<uint32>) (input: Stream) = async {
