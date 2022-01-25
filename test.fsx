ac6413ed4972bccf7ef374c8972e002c424c2e27
Tapio Malmberg <tapio.malmberg@gmail.com>
2022-01-25 22:12:55 +0200

    Update .NET version in workflows

--- test.fsx
+++ test.fsx
@@ -1,7 +1,5 @@
 #r "nuget: Fake.DotNet.Cli"
 #r "nuget: Fake.IO.FileSystem"
-// Fix: Unsupported log file format. Latest supported version is 9, the log file has version 13.
-#r "nuget: MSBuild.StructuredLogger"
 
 open Fake.DotNet
 open Fake.IO.Globbing.Operators
