a1344d3afc17ad18a80ea7142a5da71d71144694
Tapio Malmberg <tapio.malmberg@gmail.com>
2022-05-13 19:02:00 +0300

    Move audio controls to their own file

--- samples/DLCBuilder/Views/ProjectDetails.fs
+++ samples/DLCBuilder/Views/ProjectDetails.fs
@@ -2,14 +2,12 @@ module DLCBuilder.Views.ProjectDetails
 
 open Avalonia
 open Avalonia.Controls
-open Avalonia.Controls.Shapes
 open Avalonia.FuncUI.DSL
 open Avalonia.Input
 open Avalonia.Layout
 open Avalonia.Media
 open Avalonia.Media.Imaging
 open Avalonia.Platform
-open Rocksmith2014.Common
 open Rocksmith2014.DLCProject
 open System
 open DLCBuilder
@@ -18,169 +16,6 @@ open Media
 let private placeholder =
     let assets = AvaloniaLocator.Current.GetService<IAssetLoader>()
     new Bitmap(assets.Open(Uri("avares://DLCBuilder/Assets/coverart_placeholder.png")))
-
-let private audioControls state dispatch =
-    let audioPath = state.Project.AudioFile.Path
-    let previewPath = state.Project.AudioPreviewFile.Path
-    let previewExists = IO.File.Exists(previewPath)
-    let notCalculatingVolume =
-        not (state.RunningTasks |> Set.exists (function VolumeCalculation (MainAudio | PreviewAudio) -> true | _ -> false))
-
-    Border.create [
-        DockPanel.dock Dock.Top
-        Border.background "#181818"
-        Border.borderThickness 1.
-        Border.cornerRadius 6.
-        Border.margin 2.
-        Border.child (
-            Panel.create [
-                Panel.children [
-                    ViewBox.create [
-                        Viewbox.width 80.
-                        Viewbox.height 80.
-                        Viewbox.margin 12.
-                        Viewbox.verticalAlignment VerticalAlignment.Center
-                        Viewbox.horizontalAlignment HorizontalAlignment.Right
-                        Viewbox.child (
-                            Path.create [
-                                Path.data Icons.volumeHigh
-                                Path.fill "#44444444"
-                            ]
-                        )
-                    ]
-                    StackPanel.create [
-                        StackPanel.margin 4.
-                        StackPanel.children [
-                            StackPanel.create [
-                                StackPanel.orientation Orientation.Horizontal
-                                StackPanel.children [
-                                    // Main audio filename
-                                    locText "MainAudioFile" [
-                                        TextBlock.margin (4.0, 4.0, 0.0, 4.0)
-                                        TextBlock.verticalAlignment VerticalAlignment.Center
-                                    ]
-                                    TextBlock.create [
-                                        TextBlock.margin (4.0, 4.0, 0.0, 4.0)
-                                        TextBlock.verticalAlignment VerticalAlignment.Center
-                                        TextBlock.text (
-                                            if String.notEmpty audioPath then
-                                                IO.Path.GetFileName(audioPath)
-                                            else
-                                                translate "NoAudioFile"
-                                        )
-                                    ]
-                                ]
-                            ]
-
-                            StackPanel.create [
-                                StackPanel.orientation Orientation.Horizontal
-                                StackPanel.children [
-                                    // Main volume
-                                    FixedNumericUpDown.create [
-                                        NumericUpDown.minWidth 130.
-                                        NumericUpDown.margin (2.0, 2.0, 2.0, 2.0)
-                                        NumericUpDown.minimum -45.
-                                        NumericUpDown.maximum 45.
-                                        NumericUpDown.increment 0.5
-                                        NumericUpDown.formatString "+0.0;-0.0;0.0"
-                                        NumericUpDown.isEnabled (not <| state.RunningTasks.Contains(VolumeCalculation MainAudio))
-                                        FixedNumericUpDown.value state.Project.AudioFile.Volume
-                                        FixedNumericUpDown.onValueChanged (SetAudioVolume >> EditProject >> dispatch)
-                                        ToolTip.tip (translate "AudioVolumeToolTip")
-                                    ]
-
-                                    // Select audio file
-                                    Button.create [
-                                        Button.content (
-                                            PathIcon.create [
-                                                PathIcon.data Icons.folderOpen
-                                            ])
-                                        Button.isEnabled notCalculatingVolume
-                                        Button.classes [ "borderless-btn" ]
-                                        Button.onClick (fun _ -> Dialog.AudioFile false |> ShowDialog |> dispatch)
-                                        ToolTip.tip (translate "SelectAudioFile")
-                                    ]
-
-                                    Menus.audio notCalculatingVolume state dispatch
-                                ]
-                            ]
-
-                            StackPanel.create [
-                                StackPanel.orientation Orientation.Horizontal
-                                StackPanel.children [
-                                    // Preview audio filename
-                                    locText "Preview" [
-                                        TextBlock.margin (4.0, 4.0, 0.0, 4.0)
-                                    ]
-                                    TextBlock.create [
-                                        TextBlock.margin (4.0, 4.0, 0.0, 4.0)
-                                        TextBlock.text (
-                                            if String.notEmpty previewPath then
-                                                IO.Path.GetFileName(previewPath)
-                                            else
-                                                translate "NoAudioFile"
-                                        )
-                                    ]
-                                ]
-                            ]
-
-                            StackPanel.create [
-                                StackPanel.orientation Orientation.Horizontal
-                                StackPanel.children [
-                                    // Preview audio volume
-                                    FixedNumericUpDown.create [
-                                        NumericUpDown.minWidth 130.
-                                        NumericUpDown.margin (2.0, 2.0, 2.0, 2.0)
-                                        NumericUpDown.horizontalAlignment HorizontalAlignment.Left
-                                        NumericUpDown.minimum -45.
-                                        NumericUpDown.maximum 45.
-                                        NumericUpDown.increment 0.5
-                                        NumericUpDown.formatString "+0.0;-0.0;0.0"
-                                        NumericUpDown.isEnabled (not <| state.RunningTasks.Contains(VolumeCalculation PreviewAudio))
-                                        FixedNumericUpDown.value state.Project.AudioPreviewFile.Volume
-                                        FixedNumericUpDown.onValueChanged (SetPreviewVolume >> EditProject >> dispatch)
-                                        ToolTip.tip (translate "PreviewAudioVolumeToolTip")
-                                    ]
-
-                                    // Select preview audio file
-                                    Button.create [
-                                        Button.content (
-                                            PathIcon.create [
-                                                PathIcon.data Icons.folderOpen
-                                            ])
-                                        Button.isEnabled notCalculatingVolume
-                                        Button.classes [ "borderless-btn" ]
-                                        Button.onClick (fun _ -> Dialog.PreviewFile |> ShowDialog |> dispatch)
-                                        ToolTip.tip (translate "SelectPreviewAudioFile")
-                                    ]
-
-                                    // Create preview audio
-                                    Button.create [
-                                        Button.content (
-                                            hStack [
-                                                PathIcon.create [
-                                                    PathIcon.data Icons.scissors
-                                                ]
-                                                locText "Create..." [ TextBlock.margin (8., 0., 0., 0.) ]
-                                            ])
-                                        Button.classes [ "borderless-btn" ]
-                                        Button.isEnabled (IO.File.Exists(audioPath))
-                                        Button.onClick (fun _ -> dispatch (CreatePreviewAudio InitialSetup))
-                                        ToolTip.tip (
-                                            if previewExists then
-                                                translate "PreviewAudioExistsToolTip"
-                                            else
-                                                translate "PreviewAudioDoesNotExistToolTip"
-                                        )
-                                    ]
-                                ]
-                            ]
-                        ]
-                    ]
-                ]
-            ]
-        )
-    ]
 
 let private buildControls state dispatch =
     let canBuild = StateUtils.canBuild state
@@ -436,7 +271,7 @@ let view state dispatch =
 
             projectInfo state dispatch
 
-            audioControls state dispatch
+            AudioControls.view state dispatch
 
             buildControls state dispatch
         ]
