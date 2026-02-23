3315f939ade188971b9984c322b7dd1d9b3bc429
Tapio Malmberg <tapio.malmberg@gmail.com>
2026-02-23 21:10:45 +0200

    Make FSharpJsonOptions class static

--- src/Rocksmith2014.Common/FSharpJsonOptions.fs
+++ src/Rocksmith2014.Common/FSharpJsonOptions.fs
@@ -3,7 +3,8 @@ namespace Rocksmith2014.Common
 open System.Text.Json
 open System.Text.Json.Serialization
 
-type FSharpJsonOptions() =
+[<AbstractClass; Sealed>]
+type FSharpJsonOptions =
     static member Create(?indent, ?ignoreNull) =
         let ignoreNull = defaultArg ignoreNull false
 
